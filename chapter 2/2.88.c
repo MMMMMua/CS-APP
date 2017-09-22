73b875169701c6a0c9ce717433bc19fbc8f29ae3
MMMMMua <969765303@qq.com>
2017-09-22 23:02:06 +0800

    finish homework 1

--- chapter 2/2.88.c
+++ chapter 2/2.88.c
@@ -4,19 +4,20 @@
 #include <limits.h>
 #include <math.h>
 
-float a[512], b[512];
+float a[512], b[512], c[512];
 
-/* this program does not follow the floating arithmetic rule*/
+/* this program does not follow the floating arithmetic rule. */
+/* this program does not gurantee to give correct answer when a number equals to INF or NAN. */
 
 float transfer(unsigned x, int e, int f) {
 	unsigned sign = x >> (e+f) & 1;
 	unsigned exp = (x >> f) & ((1 << e) - 1);
 	unsigned frac = x & ((1 << f) - 1);
-	int E = exp - ((1 << (e - 1)) - 1);
+	int E = (int)exp - ((1 << (e - 1)) - 1);
 	int s = sign ? -1 : 1;
 	
 	if (exp == 0)
-		return s * (float)frac / (1 << f);
+		return s * (float)pow(2, E) * (float)frac / (1 << f);
 	else if (exp == (1 << e) - 1U)
 		if (frac == 0)
 			return s / 0.0;
@@ -31,4 +32,17 @@ int main() {
 		a[i] = transfer(i, 5, 3), b[i] = transfer(i, 4, 4);
 		printf("A:%.10lf   B:%.10lf\n", a[i], b[i]);
 	}
+
+	for (unsigned i = 9; i < (1 << 9); ++i) {
+		float mindif = 1 / 0.0, num = NAN;
+		for (unsigned j = 0; j < (1 << 9); ++j)
+			if (fabs(a[i] - b[j]) < mindif){
+				mindif = fabs(a[i] - b[j]);
+				num = b[j];
+			}
+		c[i] = num;
+	}
+
+	for (int i = 0; i < (1 << 9); ++i)
+		printf("the closet number to %.10f is %.10f\n", a[i], c[i]);
 }
