80a969f6ae38a76b037b8c47d9002db6b6f10177
MMMMMua <969765303@qq.com>
2017-09-24 20:36:39 +0800

    fixed some mistakes

--- chapter 2/2.88.c
+++ chapter 2/2.88.c
@@ -4,10 +4,12 @@
 #include <limits.h>
 #include <math.h>
 
-float a[512], b[512], c[512];
+float a[512], b[512];
+int c[512];
 
 /* this program does not follow the floating arithmetic rule. */
 /* this program does not gurantee to give correct answer when a number equals to INF or NAN. */
+
 
 float transfer(unsigned x, int e, int f) {
 	unsigned sign = x >> (e+f) & 1;
@@ -17,7 +19,7 @@ float transfer(unsigned x, int e, int f) {
 	int s = sign ? -1 : 1;
 	
 	if (exp == 0)
-		return s * (float)pow(2, E) * (float)frac / (1 << f);
+		return s * (float)pow(2, E+1) * (float)frac / (1 << f);
 	else if (exp == (1 << e) - 1U)
 		if (frac == 0)
 			return s / 0.0;
@@ -30,19 +32,33 @@ float transfer(unsigned x, int e, int f) {
 int main() {
 	for (unsigned i = 0; i < (1 << 9); ++i) {
 		a[i] = transfer(i, 5, 3), b[i] = transfer(i, 4, 4);
-		printf("A:%.10lf   B:%.10lf\n", a[i], b[i]);
+		/* printf("A:%.10lf   B:%.10lf\n", a[i], b[i]); */
 	}
 
-	for (unsigned i = 9; i < (1 << 9); ++i) {
-		float mindif = 1 / 0.0, num = NAN;
+	for (unsigned i = 0; i < (1 << 9); ++i) {
+		float mindif = 1e20, num = 0x0f0;
 		for (unsigned j = 0; j < (1 << 9); ++j)
-			if (fabs(a[i] - b[j]) < mindif){
-				mindif = fabs(a[i] - b[j]);
-				num = b[j];
+			if (b[j] >= a[i] && b[j] - a[i] < mindif){
+				mindif = b[j] - a[i];
+				num = j;
 			}
 		c[i] = num;
 	}
 
-	for (int i = 0; i < (1 << 9); ++i)
-		printf("the closet number to %.10f is %.10f\n", a[i], c[i]);
+	/* for (int i = 0; i < (1 << 9); ++i) */
+	/* 	printf("the closet number to %.10f is %.10f\n", a[i], c[i]); */
+
+	int table[] = {0x171, 0x0b5, 0x13e, 0x005, 0x1d8, 0x0c4};
+
+	for (int i = 0; i < 6; ++i)
+		printf("the closet number to %20.10f is %03x %20.10f\n", a[table[i]], c[table[i]], b[c[table[i]]]);
+	
+/**********************************result*********************************/
+/* the closet number to        -0.5625000000 is 162        -0.5625000000 */
+/* the closet number to       208.0000000000 is 0ea       208.0000000000 */
+/* the closet number to        -0.0068359375 is 107        -0.0068359375 */
+/* the closet number to         0.0000381470 is 001         0.0009765625 */
+/* the closet number to     -4096.0000000000 is 1ef      -248.0000000000 */
+/* the closet number to       768.0000000000 is 0f0                  inf */
+	
 }
